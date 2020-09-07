fef6a7ad51ed45947c2e50464dc9f76cc1969558
VladimirImir <VladimirImir>
2020-09-07 22:55:41 +0300

    iterator done

--- DataContainers/List/main.cpp
+++ DataContainers/List/main.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include<iostream>
 using std::cin;
 using std::cout;
 using std::endl;
+
+#define tab "\t"
 
 class List
 {
@@ -20,71 +22,287 @@ class List
 			cout << "EDestructor:\t" << this << endl;
 		}
 		friend class List;
-	};
+	};// *Head, *Tail;
 	Element* Head;
 	Element* Tail;
 	int size;
 public:
+
+	class Iterator
+	{
+		Element* Temp;
+	public:
+		Iterator(Element* Temp = nullptr) :Temp(Temp)
+		{
+			cout << "ItConstructor:\t" << this << endl;
+		}
+		~Iterator()
+		{
+			cout << "ItDestructor:\t" << this << endl;
+		}
+		Iterator& operator++()
+		{
+			Temp = Temp->pNext;
+			return *this;
+		}
+		Iterator& operator--()
+		{
+			Temp = Temp->pPrev;
+			return *this;
+		}
+
+		const int& operator*()const
+		{
+			return Temp->Data;
+		}
+		int& operator*()
+		{
+			return Temp->Data;
+		}
+
+		bool operator!=(const Iterator& other)const
+		{
+			return this->Temp != other.Temp;
+		}
+
+		operator bool()const
+		{
+			return Temp;
+		}
+	};
+
+	const Iterator begin()const
+	{
+		return this->Head;
+	}
+
+	const Iterator end()const
+	{
+		return nullptr;
+	}
+
 	List()
 	{
 		Head = Tail = nullptr;
 		size = 0;
 		cout << "LConstructor:\t" << this << endl;
 	}
+	List(const std::initializer_list<int>& il) :List()
+	{
+		for (const int* it = il.begin(); it != il.end(); it++)
+		{
+			push_back(*it);
+		}
+		cout << "ILConstructor:\t" << this << endl;
+	}
+	List(const List& other) :List()
+	{
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			push_back(Temp->Data);
+		cout << "CopyConstructor:\t" << this << endl;
+	}
 	~List()
 	{
+		//while (Head)pop_front();
+		while (Tail)pop_back();
 		cout << "LDestructor:\t" << this << endl;
 	}
 
-	// Adding elements:
+	//			Operators:
+	List& operator=(const List& other)
+	{
+		if (this == &other)return *this;
+		//while (Head)pop_front();
+		this->~List();
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			push_back(Temp->Data);
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+
+
+	//			Adding elements:
 	void push_front(int Data)
 	{
 		/*Element* New = new Element(Data);
 		if (Head == nullptr && Tail == nullptr)
 		{
-		Head = Tail = New;
-		return;
+			Head = Tail = New;
+			size++;
+			return;
 		}
 		New->pNext = Head;
 		Head->pPrev = New;
-		Head = New;*/
+		Head = New;
+		size++;*/
+
 		if (Head == nullptr && Tail == nullptr)
 			Head = Tail = new Element(Data);
 		else
 			Head = Head->pPrev = new Element(Data, Head);
+		size++;
+		//////////////////////////////////////////////////
 	}
-	void pus_back(int Data)
+	void push_back(int Data)
 	{
-
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = new Element(Data);
+		}
+		else
+		{
+			/*Element* New = new Element(Data);
+			New->pPrev = Tail;
+			Tail->pNext = New;
+			Tail = New;*/
+			Tail = Tail->pNext = new Element(Data, nullptr, Tail);
+		}
+		size++;
 	}
+
+	//			Removing elements:
+
+	void pop_front()
+	{
+		if (Head == nullptr && Tail == nullptr)return;
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+		}
+		else
+		{
+			Head = Head->pNext;
+			delete Head->pPrev;
+			Head->pPrev = nullptr;
+		}
+		size--;
+	}
+
+	void pop_back()
+	{
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+		}
+		else
+		{
+			Tail = Tail->pPrev;
+			delete Tail->pNext;
+			Tail->pNext = nullptr;
+		}
+		size--;
+	}
+
+	void erase(int Index)
+	{
+		if (Index == 0)
+		{
+			pop_front();
+			return;
+		}
+		if (Index == size - 1)
+		{
+			pop_back();	//TODO
+			return;
+		}
+		if (Index >= size)
+		{
+			return;
+		}
+		Element* Temp;
+		if (Index < size / 2)
+		{
+			//Если, элемент ближе к началу списка, 
+			//то заходим с головы, и идем от начала списка до нужного элемента.
+			Temp = Head;
+			for (int i = 0; i < Index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			//Иначе, заходим через Хвост, и движемся по списку в обратном направлении, 
+			//от конца, до нужного элемента.
+			Temp = Tail;
+			for (int i = 0; i < size - Index - 1; i++)Temp = Temp->pPrev;
+		}
+		//Не важно, как мы дошли до элемента, процедура удаления будет одна:
+		Temp->pPrev->pNext = Temp->pNext;	//В pNext предыдущего элемента от Temp (Temp->pPrev) записываем адрес элемента, следующего за Temp
+		Temp->pNext->pPrev = Temp->pPrev;
+		delete Temp;
+		size--;
+	}
+
+	//			Methods:
+
 	void print()
 	{
-		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+		//for (Element* Temp = Head; Temp; Temp++/*Temp = Temp->pNext*/)
+		for(Iterator it = Head; it; ++it) // потом доделать
 		{
-			cout << Temp->pPrev << "\t" << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << endl;
+			//cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+			cout << *it << "\t"; // потом доделать
 		}
-		cout << "List size:\t" << this << endl;
+		cout << "List size: \t" << size << endl;
 	}
-	void print_revrs()
+	void print_reverse()
 	{
-		for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
+		for (Element* Temp = Tail; Temp; Temp++/*Temp = Temp->pPrev*/)
 		{
-			cout << Temp->pPrev << "\t" << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << endl;
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 		}
-		cout << "List size:\t" << this << endl;
+		cout << "List size: \t" << size << endl;
 	}
 };
 
+//#define BASE_CHECK
+//#define COPY_METHODS
+
 void main()
 {
-	setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "Russian");
+#ifdef BASE_CHECK
 	int n;
 	cout << "Input list size: "; cin >> n;
 	List list;
+	list.pop_front();
 	for (int i = 0; i < n; i++)
 	{
-		list.push_front(rand() % 100);
+		list.push_back(rand() % 100);
 	}
 	list.print();
-	list.print_revrs();
+	//list.pop_front();
+	list = list;
+	list.print_reverse();
+#endif // BASE_CHECK
+
+
+	/*int index;
+	cout << "Type index of deleting element: "; cin >> index;
+	list.erase(index);
+
+	list.print();
+	list.print_reverse();*/
+
+#ifdef COPY_METHODS
+	cout << "\n===============================================\n";
+	List list2;
+	list2.push_back(123);
+	list2.push_back(234);
+	list2.push_back(345);
+	cout << "\n-----------------------------------------------\n";
+	list2 = list;
+	cout << "\n-----------------------------------------------\n";
+	list2.print();
+	list2.print_reverse();
+	cout << "\n===============================================\n";
+#endif // COPY_METHODS
+
+	List list = { 3, 5, 8, 13, 21 };
+	list.print();
+	/*for (int i : list)
+	{
+		cout << i << tab;
+	}
+	cout << endl;*/
 }
